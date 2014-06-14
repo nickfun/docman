84b09bbedc62c21383e1973b4b26159f437570ce
Nick F <nick@nick.gs>
2014-06-14 14:37:11 -0700

    Backbone view for a list of options

--- view-form.php
+++ view-form.php
@@ -6,12 +6,53 @@ require_once 'view-header.php';
 
 <ul id="main"></ul>
 
+<script type="text/template" id="tpl-optionlist">
+    <li class="option">
+        <label>
+        <input type="checkbox" name="option[<%- id %>]">
+        <%- title %>
+        </label>
+    </li>
+</script>
+
 <script>
 
     var DATA = <?= json_encode($DATA, JSON_PRETTY_PRINT) ?>;
 
     var groups = new Backbone.Collection(DATA.groups);
     var options = new Backbone.Collection(DATA.options);
+
+    var OptionListView = Backbone.View.extend({
+        tpl: _.template($("#tpl-optionlist").text()),
+        tagName: 'ul',
+        className: 'option-list',
+        render: function() {
+            this.$el.empty();
+            var that = this;
+            this.collection.each(function(row) {
+                var result = that.tpl(row.attributes);
+                console.log("Result: ", result);
+                that.$el.append(result);
+            });
+            return this;
+        }
+    });
+    
+    var GroupListView = Backbone.View.extend({
+        tpl: _.template($("#tpl-grouplist").text()),
+        tagName: 'ul',
+        className: 'group-list',
+        render: function() {
+            this.$el.empty();
+            var that = this;
+            
+            return this;
+        }
+    });
+
+    var z = new OptionListView({
+        collection: options
+    });
 
     function optionList(options) {
         var $list = $("<ul></ul>");
